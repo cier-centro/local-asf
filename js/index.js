c89b1a3167b6afe49b68b0155484ec670c13ab71
YesidDaza <yiye_91@hotmail.com>
2018-07-12 10:44:42 -0500

    Code refactor

--- js/index.js
+++ js/index.js
@@ -1,24 +1,13 @@
-function load() {
-    var subjectArray= [];
-    var gradeArray= [];
-    console.log(paths);
-    //console.log(paths.GRADO_7.CIENCIAS.VIDEOS.BIMESTRE_1);
-    $.each(paths, function(index, object) {
-        gradeArray.push(index);
-        for (var property in object) {
-            if($.inArray(property, subjectArray) === -1) subjectArray.push(property);
-        }
-    });
-    console.log(gradeArray);
-    console.log(subjectArray);
+$(document).ready(function(){
+    createSubjectButtons();
+});
+function createSubjectButtons() {
     for (var i = 0; i < subjectArray.length; i++) {
-      var buttoncontainer = "<div class = 'subject-button-container-" + subjectArray[i].toLowerCase() + "'></div>";
-      $( buttoncontainer ).appendTo('div.subject-buttons');
-      $('<a>',{
-        class: 'btn btn-primary',
-        text: subjectArray[i],
-        href: getSubjectURL(subjectArray[i]),
-      }).appendTo( 'div.subject-button-container-' + subjectArray[i].toLowerCase());
+      var buttonContainerClass = 'subject-button-container-'+ subjectArray[i].toLowerCase();
+      var buttonContainer = "<div class = '" + buttonContainerClass + "'></div>";
+      $( buttonContainer ).appendTo('div.subject-buttons');
+      var linkButton = getSubjectURL(subjectArray[i]);
+      createLinks('btn btn-primary', subjectArray[i], linkButton, buttonContainerClass);
     }
 }
 function getSubjectURL(subject) {
